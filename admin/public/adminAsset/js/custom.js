a78466e23f50ce5f9487f92032f4287b77aed237
Jobair Hossain <jobairh999@gmail.com>
2022-11-04 00:36:34 +0600

    update-5 again
    update-5 again

--- admin/public/adminAsset/js/custom.js
+++ admin/public/adminAsset/js/custom.js
@@ -1,59 +1,58 @@
-$(document).ready(function () {
-$('#VisitorDt').DataTable();
-$('.dataTables_length').addClass('bs-select');
+
+// Visitor Page Table
+$(document).ready(function() {
+    $('#VisitorDt').DataTable();
+    $('.dataTables_length').addClass('bs-select');
 });
 
-
- function getServicesData() {
+// For Services Table
+function getServicesData() {
     axios.get('/getServicesData')
 
-    .then(function (response) {
+        .then(function(response) {
 
-        if (response.status===200){
+            if (response.status === 200) {
 
-            $('#mainDiv').removeClass('d-none');
-            $('#loaderDiv').addClass('d-none');
+                $('#mainDiv').removeClass('d-none');
+                $('#loaderDiv').addClass('d-none');
 
-            $('#service_table').empty();
+                $('#service_table').empty();
 
-            let jsonData=response.data;
+                let jsonData = response.data;
 
-            $.each(jsonData,function (i,item) {
-                $('<tr>').html(
-                    "<td><img class='table-img' src="+jsonData[i].service_image +"></td>" +
-                    "<td>"+ jsonData[i].service_name +"</td>" +
-                    "<td>"+ jsonData[i].service_description +"</td>" +
-                    "<td><a href='' ><i class='fas fa-edit'></i></a> </td>" +
-                    "<td><a class='serviceDeleteBtn' data-id="+jsonData[i].id +" ><i class='fas fa-trash-alt'></i></a> </td>"
-                ).appendTo('#service_table');
-            });
+                $.each(jsonData, function(i, item) {
+                    $('<tr>').html(
+                        "<td><img class='table-img' src=" + jsonData[i].service_image + "></td>" +
+                        "<td>" + jsonData[i].service_name + "</td>" +
+                        "<td>" + jsonData[i].service_description + "</td>" +
+                        "<td><a href='' ><i class='fas fa-edit'></i></a> </td>" +
+                        "<td><a class='serviceDeleteBtn' data-id=" + jsonData[i].id + " ><i class='fas fa-trash-alt'></i></a> </td>"
+                    ).appendTo('#service_table');
+                });
 
-            $('.serviceDeleteBtn').click(function () {
-                let id=$(this).data('id');
+                // Services Table Delete icon click
+                $('.serviceDeleteBtn').click(function() {
+                    let id = $(this).data('id');
 
-                $('#serviceDeleteId').html(id);
-                $('#deleteModal').modal('show');
-            })
+                    $('#serviceDeleteId').html(id);
+                    $('#deleteModal').modal('show');
+                })
 
+                // Services Delete Modal Yes Btn
+                $('#serviceDeleteConfirmBtn').click(function() {
+                    let id = $('#serviceDeleteId').html();
+                    getServicesDelete(id);
+                })
+            } else {
 
+                $('#wrongDiv').removeClass('d-none');
+                $('#loaderDiv').addClass('d-none');
 
-            $('#serviceDeleteConfirmBtn').click(function () {
-                let id=$('#serviceDeleteId').html();
-                getServicesDelete(id);
-            })
-        }
-
-
-        else {
-
-            $('#wrongDiv').removeClass('d-none');
-            $('#loaderDiv').addClass('d-none');
-
-        }
+            }
 
 
-    })
-        .catch(function (error) {
+        })
+        .catch(function(error) {
 
             $('#wrongDiv').removeClass('d-none');
             $('#loaderDiv').addClass('d-none');
@@ -61,23 +60,23 @@ $('.dataTables_length').addClass('bs-select');
         });
 }
 
+// Services Delete
 function getServicesDelete(deleteId) {
-    axios.post('/serviceDelete',{id:deleteId})
-    .then(function (response) {
-        if (response.data===1){
-            $('#deleteModal').modal('hide');
-            toastr.success('Delete Success  ');
-            getServicesData();
-        }
-        else {
-            $('#deleteModal').modal('hide');
-            toastr.error('Delete Fail');
-            getServicesData();
-        }
+    axios.post('/serviceDelete', {
+        id: deleteId
     })
-        .catch(function (error) {
+        .then(function(response) {
+            if (response.data === 1) {
+                $('#deleteModal').modal('hide');
+                toastr.success('Delete Success  ');
+                getServicesData();
+            } else {
+                $('#deleteModal').modal('hide');
+                toastr.error('Delete Fail');
+                getServicesData();
+            }
+        })
+        .catch(function(error) {
 
         })
 }
-
-
